e47df21d49bdca0bc44a0119ab9acc111c48dc56
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-04-02 19:42:36 +0300

    Add comments

--- passwordChecker.cpp
+++ passwordChecker.cpp
@@ -5,14 +5,17 @@
 
 using namespace std;
 
+// Путь к файлу, где по умолчанию лежит пароль (локально, в той же папке)
 #define PATH_TO_FILE_WITH_PASSWORD "password.txt"
+// Идентификатор кодировки Windows 1251 с русской локализацией
 #define RUS_ENCODING 1251
 
 int main(void) {
 	SetConsoleCP(RUS_ENCODING);
 	SetConsoleOutputCP(RUS_ENCODING);
 
-	string password, correctPassword;
+	// Пароль, вводимый пользователем, и корректный пароль, который берется из файла
+	string password, correctPassword; 
 	cout << "Введите пароль: ";
 	cin >> password;
 
