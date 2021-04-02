9653a19901f2811bb00cc53d97a38443259c32f5
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-04-02 19:58:10 +0300

    Added password XOR-encryption

--- passwordChecker.cpp
+++ passwordChecker.cpp
@@ -2,27 +2,41 @@
 #include <fstream>
 #include <Windows.h>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
 // Путь к файлу, где по умолчанию лежит пароль (локально, в той же папке)
-#define PATH_TO_FILE_WITH_PASSWORD "password.txt"
+#define PATH_TO_FILE_WITH_PASSWORD "encryptedPassword.txt"
 // Идентификатор кодировки Windows 1251 с русской локализацией
 #define RUS_ENCODING 1251
+// Случайное число для шифрования пароля с помощью XOR
+#define XOR_KEY 521
+
+// Шифруем строку с помощью XOR-шифрования
+string encryptString(string s) {
+	for (size_t i = 0; i < s.length(); i++) {
+		s[i] ^= XOR_KEY;
+	}
+
+	return s;
+}
 
 int main(void) {
 	SetConsoleCP(RUS_ENCODING);
 	SetConsoleOutputCP(RUS_ENCODING);
 
-	// Пароль, вводимый пользователем, и корректный пароль, который берется из файла
-	string password, correctPassword; 
+	// Пароль, вводимый пользователем, зашифрованный пользовательский пароль и корректный пароль (зашифрованный), который берется из файла
+	string password, encryptedPassword, encryptedCorrectPassword;
 	cout << "Введите пароль: ";
 	cin >> password;
+	encryptedPassword = encryptString(password);
+	
 
 	ifstream fileWithPassword(PATH_TO_FILE_WITH_PASSWORD);
-	getline(fileWithPassword, correctPassword);
+	getline(fileWithPassword, encryptedCorrectPassword);
 
-	if (password.compare(correctPassword) != 0) {
+	if (encryptedPassword.compare(encryptedCorrectPassword) != 0) {
 		cout << "Введен неверный пароль!" << endl;
 		exit(1);
 	}
